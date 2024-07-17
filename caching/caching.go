deef16ec389de744df6ea1728816399836b58db9
Jonas Roussel <dev@jonas.bzh>
2024-07-17 19:27:10 +0200

    feat(cert): adding certificate caching

---
+++ caching/caching.go
@@ -0,0 +1,73 @@
+package caching
+
+import (
+	"container/list"
+	"crypto/tls"
+	"errors"
+
+	"github.com/jonasroussel/hyve/stores"
+)
+
+var cachingQueue = list.New()
+
+type CahcedData struct {
+	sni         string
+	certificate *tls.Certificate
+}
+
+func CertificateRetriever(chi *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	if chi.ServerName == "" {
+		return nil, errors.New("server name (sni) is empty")
+	}
+
+	cert := loadFromCache(chi.ServerName)
+	if cert != nil {
+		return cert, nil
+	}
+
+	cert, err := loadFromStore(chi.ServerName)
+	if err != nil {
+		return nil, err
+	}
+
+	addToCache(chi.ServerName, cert)
+
+	return cert, nil
+}
+
+func loadFromStore(sni string) (*tls.Certificate, error) {
+	cert, err := stores.Active.GetCertificate(sni)
+	if err != nil {
+		return nil, err
+	}
+
+	x509Cert, err := tls.X509KeyPair(cert.CertificateData, cert.PrivateKeyData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &x509Cert, nil
+}
+
+func loadFromCache(sni string) *tls.Certificate {
+	for e := cachingQueue.Front(); e != nil; e = e.Next() {
+		cert := e.Value.(CahcedData)
+		if cert.sni == sni {
+			return cert.certificate
+		}
+	}
+
+	return nil
+}
+
+func addToCache(sni string, cert *tls.Certificate) {
+	cachingQueue.PushFront(CahcedData{
+		sni:         sni,
+		certificate: cert,
+	})
+
+	// Remove the oldest certificate if the cache is too big
+	if cachingQueue.Len() > 1000 {
+		cachingQueue.Remove(cachingQueue.Back())
+	}
+}
--- servers/server.go
+++ servers/server.go
@@ -2,20 +2,17 @@ package servers
 
 import (
 	"crypto/tls"
-	"errors"
 	"log"
 	"net"
 	"net/http"
 
-	"github.com/jonasroussel/hyve/stores"
+	"github.com/jonasroussel/hyve/caching"
 )
 
 func NewTLS() (net.Listener, *http.Server, *http.ServeMux) {
 	handler := http.NewServeMux()
 
-	listener, err := tls.Listen("tcp", ":443", &tls.Config{
-		GetCertificate: getCertificate,
-	})
+	listener, err := tls.Listen("tcp", ":443", &tls.Config{GetCertificate: caching.CertificateRetriever})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,23 +33,4 @@ func NewHTTP() (net.Listener, *http.Server, *http.ServeMux) {
 	server := &http.Server{Handler: handler}
 
 	return listener, server, handler
-}
-
-// TODO : maybe need to be cached
-func getCertificate(chi *tls.ClientHelloInfo) (*tls.Certificate, error) {
-	if chi.ServerName == "" {
-		return nil, errors.New("server name (sni) is empty")
-	}
-
-	cert, err := stores.Active.GetCertificate(chi.ServerName)
-	if err != nil {
-		return nil, err
-	}
-
-	x509Cert, err := tls.X509KeyPair(cert.CertificateData, cert.PrivateKeyData)
-	if err != nil {
-		return nil, err
-	}
-
-	return &x509Cert, nil
 }
